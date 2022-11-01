98d1ba4db95ea78f614637fe33656bd76eaedbf6
Kamelia Laksi <klaksi@c1r8p3.42quebec.com>
2022-11-01 11:39:40 -0400

    add ft_calloc, ft_substr

--- src/ft_mem/ft_calloc.c
+++ src/ft_mem/ft_calloc.c
@@ -1,33 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: klaksi <klaksi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/30 17:26:09 by kamelialaks       #+#    #+#             */
-/*   Updated: 2022/10/31 11:22:05 by klaksi           ###   ########.fr       */
+/*   Created: 2022/10/28 12:44:51 by klaksi            #+#    #+#             */
+/*   Updated: 2022/11/01 11:31:57 by klaksi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "../include/libft.h"
+#include "../../include/libft.h"
 
-// void	*ft_memset(void *b, int c, size_t len)
-// {
-//     char *var = (char *)b;
-//     int  i;
-//     int j = ft_strlen(var);
-
-//     i = 0;
-//     // b = (char *)b;
-//     printf("j: %d\n", j);
-//     while(j && len)
-// 	{
-// 		var[i] = (unsigned char)c;
-// 		j--;
-// 		len--;
-// 		i++;
-// 	}
-//     var[i + j] = '\0';
-//     return ((void *)b);
-// }
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t		memo;
+	char	*pointer;
+	size_t	i;
+	
+	memo = count * size;
+	pointer = malloc(memo * sizeof(char));
+	i = 0;
+	if (!pointer)
+	{
+		return (NULL);
+	}
+	while (i < memo)
+	{
+		pointer[i] = 0;
+		i++;
+	}
+	return ((void *)pointer);
+}
---
+++ src/ft_str/ft_strdup.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klaksi <klaksi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/28 18:28:53 by klaksi            #+#    #+#             */
+/*   Updated: 2022/11/01 11:32:32 by klaksi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char    *ptr;
+
+    ptr = malloc (ft_strlen(s1) * sizeof(char) + 1);
+    if (!ptr)
+    {
+        return (NULL);
+    }
+    ft_strlcpy(ptr, s1, ft_strlen(s1) + 1);
+    return (ptr);
+}
